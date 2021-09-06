7190d74674ded2dff66b505276f183088ed57096
Sarangt123 (sarangt123) <sarang.thekkedathpr@gmail.com>
2021-09-06 20:08:38 +0530

    version fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
     name='augmented',
-    version='2.1.0Stable',
+    version='2.1.0',
     description='Augmented reality in python made easy',
     long_description=open('README.txt').read() + '\n\n' +
     open('CHANGELOG.txt').read(),
